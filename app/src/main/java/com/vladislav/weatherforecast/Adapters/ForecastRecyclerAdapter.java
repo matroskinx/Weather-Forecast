e811a19257d8dfe220c17a7a5028e6726bf03bb9
Vladislav Kirianov <matroskinx@gmail.com>
2019-05-09 20:31:48 +0300

    add icons for weather conditions

--- app/src/main/java/com/vladislav/weatherforecast/Adapters/ForecastRecyclerAdapter.java
+++ app/src/main/java/com/vladislav/weatherforecast/Adapters/ForecastRecyclerAdapter.java
@@ -3,6 +3,7 @@ package com.vladislav.weatherforecast.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.vladislav.weatherforecast.Model.Forecast;
@@ -42,6 +43,7 @@ public class ForecastRecyclerAdapter extends RecyclerView.Adapter<ForecastRecycl
     class ForecastHolder extends RecyclerView.ViewHolder {
         View view;
         ListItem forecastItem;
+        ImageView forecastIcon;
         TextView forecastDesc;
         TextView forecastText;
         TextView forecastTempHigh;
@@ -50,6 +52,7 @@ public class ForecastRecyclerAdapter extends RecyclerView.Adapter<ForecastRecycl
         ForecastHolder(View v) {
             super(v);
             this.view = v;
+            forecastIcon = view.findViewById(R.id.weather_icon);
             forecastDesc = view.findViewById(R.id.weather_desc);
             forecastText = view.findViewById(R.id.weather_date);
             forecastTempHigh = view.findViewById(R.id.weather_temp_high);
@@ -63,10 +66,52 @@ public class ForecastRecyclerAdapter extends RecyclerView.Adapter<ForecastRecycl
             String desc = forecastItem.getWeather().get(0).getDescription();
             double min = forecastItem.getMain().getTemp_min();
             double max = forecastItem.getMain().getTemp_max();
+            String weatherCondition = forecastItem.getWeather().get(0).getIcon();
+            int weatherIcon = getIconId(weatherCondition);
+
+            forecastIcon.setImageResource(weatherIcon);
             forecastText.setText(Integer.toString(dt));
             forecastDesc.setText(desc);
             forecastTempLow.setText(Double.toString(min));
             forecastTempHigh.setText(Double.toString(max));
         }
+    }
+
+    private int getIconId(String code) {
+        code = code.substring(0, 2);
+        int value;
+        switch (code) {
+            case "01":
+                value = R.drawable.ic_sunny;
+                break;
+            case "02":
+                value = R.drawable.ic_cloud;
+                break;
+            case "03":
+                value = R.drawable.ic_cloud;
+                break;
+            case "04":
+                value = R.drawable.ic_cloud;
+                break;
+            case "09":
+                value = R.drawable.ic_rain;
+                break;
+            case "10":
+                value = R.drawable.ic_rain;
+                break;
+            case "11":
+                value = R.drawable.ic_thunder;
+                break;
+            case "13":
+                value = R.drawable.ic_snow;
+                break;
+            case "50":
+                value = R.drawable.ic_mist;
+                break;
+            default:
+                value = R.drawable.ic_sunny;
+                break;
+        }
+        return value;
     }
 }
