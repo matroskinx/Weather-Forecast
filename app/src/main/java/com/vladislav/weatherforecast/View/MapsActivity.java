05daa37f769c91b0f50897c6ba7e8bf5ffa8e9cc
Vladislav Kirianov <matroskinx@gmail.com>
2019-05-12 11:49:12 +0300

    request location permission

--- app/src/main/java/com/vladislav/weatherforecast/View/MapsActivity.java
+++ app/src/main/java/com/vladislav/weatherforecast/View/MapsActivity.java
@@ -1,43 +1,47 @@
 package com.vladislav.weatherforecast.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.vladislav.weatherforecast.R;
 import com.vladislav.weatherforecast.Repository.SharedPrerencesRepository;
 import com.vladislav.weatherforecast.Viewmodel.ForecastViewModel;
-
-import static com.vladislav.weatherforecast.Repository.WeatherRepository.SP_LAT;
-import static com.vladislav.weatherforecast.Repository.WeatherRepository.SP_LNG;
-
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private LatLng currentPosition;
     private SharedPrerencesRepository sharedPrerencesRepo;
+    private FusedLocationProviderClient fusedLocationClient;
 
     public static final String LAT_KEY = "key_lat";
     public static final String LNG_KEY = "key_lng";
 
     @Override
     public boolean onSupportNavigateUp() {
-        Toast.makeText(getApplicationContext(), "Up click", Toast.LENGTH_LONG).show();
         setResult(Activity.RESULT_CANCELED);
         finish();
         return true;
@@ -51,6 +55,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -82,14 +88,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onCameraIdle() {
                 currentPosition = mMap.getCameraPosition().target;
-                mMap.addMarker(new MarkerOptions().position(currentPosition));
-            }
-        });
-
-        mMap.setOnCameraMoveStartedListener(new GoogleMap.OnCameraMoveStartedListener() {
-            @Override
-            public void onCameraMoveStarted(int i) {
-                mMap.clear();
             }
         });
 
@@ -97,12 +95,63 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             double lat = sharedPrerencesRepo.getLat();
             double lng = sharedPrerencesRepo.getLng();
             currentPosition = new LatLng(lat, lng);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
         } else {
-            //TODO get current position
-            currentPosition = new LatLng(53, 24);
+            if (hasLocationPermission()) {
+                getCurrentLocation();
+            } else {
+                requestLocationPermission();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            double lat = location.getLatitude();
+                            double lng = location.getLongitude();
+
+                            currentPosition = new LatLng(lat, lng);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+
+                        }
+                    }
+                });
+    }
+
+    private boolean hasLocationPermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, R.string.permission_rationale, Toast.LENGTH_LONG).show();
+        }
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                33
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == 33) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Permission NOT granted", Toast.LENGTH_LONG).show();
+            }
         }
 
-        mMap.addMarker(new MarkerOptions().position(currentPosition));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
